5b9b4d8efa3a3f3b5f47aba713765bee3fab36e6
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-02-21 20:43:33 +0600

    Add files via upload

---
+++ Constructor/JavaApplication86DefaultConstructor/src/javaapplication86defaultconstructor/JavaApplication86DefaultConstructor.java
@@ -0,0 +1,12 @@
+package javaapplication86defaultconstructor;
+
+/**
+ *
+ * @author Moloy
+ */
+public class JavaApplication86DefaultConstructor {
+    public static void main(String[] args) {
+        Msg m1=new Msg();
+    }
+    
+}
---
+++ Constructor/JavaApplication86DefaultConstructor/src/javaapplication86defaultconstructor/Msg.java
@@ -0,0 +1,13 @@
+
+package javaapplication86defaultconstructor;
+
+/**
+ *
+ * @author Moloy
+ */
+public class Msg {
+    Msg()
+    {
+        System.out.println("Hi!");
+    }
+}
