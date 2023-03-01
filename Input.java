8845deb8d9c29321493a055402e95bf841d4e6cd
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-03-01 18:52:46 +0600

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,18 @@
+package input;
+//import java.util.Scanner;
+import java.util.*;
+/**
+ *
+ * @author Moloy
+ */
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        float b=sc.nextFloat();
+        char c=sc.next().charAt(0); //character of 0 index
+        String s=sc.next(); //One word take input
+        String s2=sc.nextLine(); //full santance
+    }
+    
+}
