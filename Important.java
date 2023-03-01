083ffa3828e9a9b5b18cf7154a24d153bb4b27a1
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-03-01 18:23:15 +0600

    Add files via upload

---
+++ Important.java
@@ -0,0 +1,18 @@
+package important;
+
+/**
+ *
+ * @author Moloy
+ */
+public class Important {
+    public static void main(String[] args) {
+        double r;
+        r=24/23; //ans: 1
+        r=(double) 24/23; //ans: 1.0434782608695652
+        r=(double)(24/23)*4; //ans: 4
+        r=(double)(24/23)*(double)4; //ans: 4
+        r=((double)24/23)*(double)4;
+        System.out.println(r);
+    }
+    
+}
