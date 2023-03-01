1b3f97917015e013f34713fe259d795d66796899
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-03-01 22:48:29 +0600

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+package array;
+
+/**
+ *
+ * @author Moloy
+ */
+public class Array {
+    public static void main(String[] args) {
+        
+        int[] num=new int[20];
+        int n[]=new int[20];
+        int n2[];
+        n2=new int[20];
+        
+        String[] name={"Moloy","Rahul","Kabir"};
+        
+        int row=3, column=4;
+        int[][] matrix=new int[row][column];
+    }
+    
+}
